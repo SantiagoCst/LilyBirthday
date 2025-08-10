7e882829a98a0388861add673c48611c6fe8b16f
SantiagoCst <97368055+SantiagoCst@users.noreply.github.com>
2025-08-10 18:47:22 -0500

    BirthdayAppV4

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'lily-birthday-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/triples-bg.jpg',
+        '/chikibang.mp4',
+        '/logo.png',
+        '/logo-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
